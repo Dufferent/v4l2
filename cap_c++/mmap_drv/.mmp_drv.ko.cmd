53d73a63a717ea535c2e0c7a02f969e5037c1337
xny <2269969490@qq.com>
2020-10-16 19:41:38 -0700

    lcd driver mode for RGB888

---
+++ cap_c++/mmap_drv/.mmp_drv.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ts/Downloads/GEC6818模拟器/mmap_drv/mmp_drv.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/ts/Downloads/GEC6818模拟器/mmap_drv/mmp_drv.ko /home/ts/Downloads/GEC6818模拟器/mmap_drv/mmp_drv.o /home/ts/Downloads/GEC6818模拟器/mmap_drv/mmp_drv.mod.o ;  true
